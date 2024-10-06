aba97de2d3d772a4248a92e2a18e5c365e2f3143
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 07:18:45 -0400

    Added method for calculating the height of the triangle

--- inverted_right_triangle.py
+++ inverted_right_triangle.py
@@ -1,3 +1,7 @@
 #Generate an inverted right triangle
+def inverted_right_triangle(n):
+    
+    #for calculating the shape of the triangle
+    for i in range(n, 0 , -1):
+        print("*" * i)
 #add a method for getting the user input
-#add a metho for calculating the shape of the triangle
