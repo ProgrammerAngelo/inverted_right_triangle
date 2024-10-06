ae3f908fa62c706e5c5709e2feed724435a9a993
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 07:22:25 -0400

    Added method for generating the triangle

--- inverted_right_triangle.py
+++ inverted_right_triangle.py
@@ -1,7 +1,15 @@
 #Generate an inverted right triangle
 def inverted_right_triangle(n):
-    
+
     #for calculating the shape of the triangle
     for i in range(n, 0 , -1):
         print("*" * i)
-#add a method for getting the user input
+
+
+try:
+    #for getting the user input
+    height = int(input("Enter the height of the triangle: "))
+    #for generating the triangle
+    inverted_right_triangle(height)
+except ValueError as e:
+    print(e)
